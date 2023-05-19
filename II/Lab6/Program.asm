29cd700b5fe4a0e31f545f3078c08b74f84928c4
Аригуун Болорболд <bolorboldariguun16@gmail.com>
2023-05-19 13:21:20 +0300

    Update Program.asm
    removed extra whitespaces

--- II/Lab6/Program.asm
+++ II/Lab6/Program.asm
@@ -1,5 +1,5 @@
 ORG 0x027
-X:	WORD ?	
+X:	WORD ?
 CACHE:	WORD ?
 L:	WORD 0xFFEA
 R:	WORD 0x0014
@@ -8,66 +8,66 @@ V0:	WORD 	$DEF, 0x180
 V1: 	WORD 	$INT1, 0x180
 V2: 	WORD 	$INT2, 0x180
 V3: 	WORD 	$DEF, 0x180
-V4: 	WORD 	$DEF, 0x180 
+V4: 	WORD 	$DEF, 0x180
 V5: 	WORD 	$DEF, 0x180
 V6: 	WORD 	$DEF, 0x180
 V7: 	WORD 	$DEF, 0x180
 DEF:	IRET
 ORG 0x0F0
-START:	DI	 
+START:	DI
  	CLA
- 	OUT 0x1 	 
- 	OUT 0x7 	 
- 	OUT 0xB 	 
- 	OUT 0xD 	 
- 	OUT 0x11 	 
- 	OUT 0x15 	 
- 	OUT 0x19 	 
- 	OUT 0x1D	 
-	LD #0x9 
+ 	OUT 0x1
+ 	OUT 0x7
+ 	OUT 0xB
+ 	OUT 0xD
+ 	OUT 0x11
+ 	OUT 0x15
+ 	OUT 0x19
+ 	OUT 0x1D
+	LD #0x9
  	OUT 3
- 	LD #0xA 
- 	OUT 5 
+ 	LD #0xA
+ 	OUT 5
  	JUMP $PROG
-PROG:	EI	
+PROG:	EI
 	CLA
-SLP:	EI	
+SLP:	EI
 	LD $X
-	DI	
-	ADD #1
-	CMP $L	 
-	BLT MIN	
-	CMP $R	
-	BGE MIN	
-	ST $X	
-	JUMP SLP 
-MIN:	LD $L	
-	ST $X	
+	DI
+	INC
+	CMP $L
+	BLT MIN
+	CMP $R
+	BGE MIN
+	ST $X
 	JUMP SLP
-INT1:	DI	
-	LD $X	
-	ASL	
-	ASL	
-	ADD $X	
+MIN:	LD $L
+	ST $X
+	JUMP SLP
+INT1:	DI
+	LD $X
+	ASL
+	ASL
+	ADD $X
 	ADD $X
 	NEG
-	SUB #5	
-	OUT 2	
-	HLT	
-	IRET	
-INT2:	DI	
+	SUB #5
+	OUT 2
+	HLT
+	IRET
+INT2:	DI
 	CLA
-	IN 4	
-	ST $CACHE	
-	SUB $X	
-	CMP $L	
-	BLT INF	
-	CMP $R	
-	BGE INF	
-	ST $X	
-	HLT	
-	IRET	
-INF:	LD $L	
-	ST $X	
-	HLT	
+	IN 4
+	ST $CACHE
+	SUB $X
+	CMP $L
+	BLT INF
+	CMP $R
+	BGE INF
+	ST $X
+	HLT
+	IRET
+INF:	LD $L
+	ST $X
+	HLT
 	IRET
